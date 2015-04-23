7c7b582c64faad12a66159534674cf90cdf905e9
lucasvinbr <lucasvinbr@gmail.com>
2015-04-23 18:10:14 -0300

    Ajustes no controle de pulo

--- GameJam/Assets/Scripts/ControleDePulo.cs
+++ GameJam/Assets/Scripts/ControleDePulo.cs
@@ -7,51 +7,41 @@ using System.Collections;
 /// </summary>
 public class ControleDePulo : MonoBehaviour {
 
-	private CharacterController myRigid;
-	private float lastJumpHeight;
+	private CharacterController myController;
+	//private Rigidbody myRigid;
+	private Vector3 moveDir;
+	private float gravity;
 	[SerializeField]
 	private float jumpForce = 6;
 
-	private bool grounded = true;
 
 	// Use this for initialization
 	void Start () {
-		myRigid = GetComponent<CharacterController> ();
-		myRigid.attachedRigidbody.useGravity = true;
+		myController = GetComponent<CharacterController> ();
+		//myRigid = GetComponent<Rigidbody> ();
+		gravity = Physics.gravity.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//confere se teve os toque
-		myRigid.SimpleMove (Vector3.zero);
-		if(myRigid.isGrounded && (Input.touchCount > 0 || Input.GetButtonDown("Fire1"))) {
+
+		if(myController.isGrounded && (Input.touchCount > 0 || Input.GetButtonDown("Fire1"))) {
 
 			//TODO tem q conferir q nao esta apertando o botao de pause ou coisa parecida
-			//myRigid.Move(Vector3.up * jumpForce);
-			//myRigid.velocity = (Vector2.up * jumpForce);
-			grounded = false;
-			StartCoroutine(getGrounded());
+
+			moveDir.y = jumpForce;
 
 
 		}
 
-		/*if (myRigid.velocity.y == 0 || Physics.Raycast(transform.position,Vector2.up * -1, 0.8f)) {
-			if(!grounded){
-				//Debug.Log ("ground");
-				grounded = true;
+
+		moveDir.y += gravity * Time.deltaTime;
+		
+		myController.Move(moveDir * Time.deltaTime);
 
 
-			}
-
-		} else
-			grounded = false;
-
-		*/
+		
 	}
-
-	IEnumerator getGrounded(){
-		Debug.Log (Mathf.Abs((jumpForce / (Physics.gravity.y))).ToString ());
-		yield return new WaitForSeconds (Mathf.Abs((jumpForce / (Physics.gravity.y))));
-		grounded = true;
-	}
+	
 }
