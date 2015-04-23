86e87021f7733fcf8b10cf68c0ecd069708c4ddc
Lucas Sabião <lucas.sabiao@hotmail.com>
2015-04-23 15:59:37 -0300

    Adicionado Controlador do Colunas
    Adicionado um GameObject para manter e controlar as colunas

---
+++ GameJam/Assets/Scripts/ControladorColunas.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControladorColunas : MonoBehaviour {
+
+	public int velocidadeDasColunas, localDaMorte, LocalDoNascimento;
+
+	public static int velocidade;
+	public static int morte;
+	public static int nascimento;
+
+	public static int ultimaAltura,penultimaAltura,AntiPenultimaAltura;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+		velocidade = velocidadeDasColunas;
+		morte = localDaMorte;
+		nascimento = LocalDoNascimento;
+	}
+
+	public static void step(int p){
+		AntiPenultimaAltura = penultimaAltura;
+		penultimaAltura = ultimaAltura;
+		ultimaAltura = p;
+	}
+}
