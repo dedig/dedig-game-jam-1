40c8bc157a2adfb5ba630ab95339a61befe3a4d3
lucasvinbr <lucasvinbr@gmail.com>
2015-04-23 11:08:56 -0300

    Script inicial de pulo (e pasta Scripts)

---
+++ GameJam/Assets/Scripts/Pulo.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Pulo. faz o personagem pular
+/// esse script vai no gameobject do personagem (o gameobject q tiver o rigidbody dele)
+/// </summary>
+public class Pulo : MonoBehaviour {
+
+	private Rigidbody2D myRigid;
+
+	[SerializeField]
+	private float jumpForce = 6;
+
+	private bool grounded = false;
+
+	// Use this for initialization
+	void Start () {
+		myRigid = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//confere se teve os toque
+		if(grounded && (Input.touchCount > 0 || Input.GetButtonDown("Fire1"))) {
+
+			//TODO tem q conferir q nao esta apertando o botao de pause ou coisa parecida
+		
+			myRigid.velocity = (Vector2.up * jumpForce);
+
+
+		}
+
+		if (myRigid.velocity.y == 0) {
+			if(!grounded){
+				//Debug.Log ("ground");
+				grounded = true;
+			}
+
+		} else
+			grounded = false;
+	}
+}
