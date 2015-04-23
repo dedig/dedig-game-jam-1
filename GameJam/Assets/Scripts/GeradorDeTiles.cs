d2c776f6bd89eb5c05c87190635282a36152b8f2
Lucas Sabião <lucas.sabiao@hotmail.com>
2015-04-23 15:14:55 -0300

    Adionado Prefab e suas dependencias

---
+++ GameJam/Assets/Scripts/GeradorDeTiles.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeradorDeTiles : MonoBehaviour {
+
+	public int altura;
+	public int velocidade;
+	public int morrer;
+	public int nascer;
+
+	void Start () {
+		posicionar ();
+	}
+	
+	void Update () {
+		this.transform.Translate ((Vector3.left * Time.deltaTime)*velocidade);
+		if (this.transform.position.x <= morrer) {
+			respawn();
+		}
+	}
+
+	void respawn(){
+		var posicao = new Vector3(nascer,0,0);
+		this.transform.position = posicao;
+		posicionar ();
+	}
+
+	void posicionar(){
+		altura = Random.Range (0, 5);
+		this.transform.Translate (Vector3.up * altura);
+	}
+}
