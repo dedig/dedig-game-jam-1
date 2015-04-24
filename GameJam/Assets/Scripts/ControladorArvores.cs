a3fc3256cfec6db575eb60050b24a5d1bf74c0e0
lucasvinbr <lucasvinbr@gmail.com>
2015-04-24 10:09:06 -0300

    Inclusao do system.collections.generic no controladorArvores

--- GameJam/Assets/Scripts/ControladorArvores.cs
+++ GameJam/Assets/Scripts/ControladorArvores.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ControladorArvores : MonoBehaviour {
 	List<GameObject> arvores = new List<GameObject>();
