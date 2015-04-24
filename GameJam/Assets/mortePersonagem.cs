7e20e51c10f744fbc705768724a7efc6a308847c
Lucas Sabião <lucas.sabiao@hotmail.com>
2015-04-24 12:52:38 -0300

    Pontuação e Morte

---
+++ GameJam/Assets/mortePersonagem.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mortePersonagem : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (PlayerPrefs.HasKey ("best")) {
+			int oldbest = PlayerPrefs.GetInt ("best");
+			if (oldbest < ControladorColunas.pontuacao) {
+				PlayerPrefs.SetInt ("best", (int)ControladorColunas.pontuacao);
+			}
+		}
+		else{
+				PlayerPrefs.SetInt("best",0);
+			}
+
+		Application.LoadLevel(0);
+	}
+}
