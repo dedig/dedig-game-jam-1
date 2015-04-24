d12ebae713e082704affb1da760b3441df6e2afc
lucasvinbr <lucasvinbr@gmail.com>
2015-04-24 11:19:57 -0300

    Ajustes no controle de pulo

--- GameJam/Assets/Scripts/ControleDePulo.cs
+++ GameJam/Assets/Scripts/ControleDePulo.cs
@@ -12,7 +12,10 @@ public class ControleDePulo : MonoBehaviour {
 	private Vector3 moveDir;
 	private float gravity;
 	[SerializeField]
-	private float jumpForce = 6;
+	private float jumpForce = 7;
+
+	public bool isGrounded;
+	public bool pulando = false;
 
 
 	// Use this for initialization
@@ -24,24 +27,46 @@ public class ControleDePulo : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		//confere se teve os toque
+		isGrounded = myController.isGrounded;
 
-		if(myController.isGrounded && (Input.touchCount > 0 || Input.GetButtonDown("Fire1"))) {
+		if (isGrounded && pulando) {
+			pulando = false;
+			Debug.Log("cabou pulo");
+		}
+
+
+		//confere se teve os toque
+		//TODO arrumar a queda livre (nao originaria de pulo, pq ta caindo mt rapido)
+
+		if(isGrounded && (Input.touchCount > 0 || Input.GetButtonDown("Pular"))) {
 
 			//TODO tem q conferir q nao esta apertando o botao de pause ou coisa parecida
 
 			moveDir.y = jumpForce;
-
-
+			pulando = true;
+			isGrounded = false;
 		}
 
-
+		if(!isGrounded)
 		moveDir.y += gravity * Time.deltaTime;
-		
+
+
+
 		myController.Move(moveDir * Time.deltaTime);
 
-
+		//checa de novo se nao mudou a situacao do grounded
+		if (myController.isGrounded && !isGrounded && !pulando) {
+			Debug.Log("mudou");
+			isGrounded = true;
+			moveDir.y = 0;
+		}
 		
 	}
-	
+
+	/*void LateUpdate(){
+		Debug.Log ("leite");
+
+		if(isGrounded && myController.isGrounded
+	}*/
+
 }
