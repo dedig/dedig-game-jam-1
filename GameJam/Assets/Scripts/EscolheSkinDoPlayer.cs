20e1eeede7ebe22612e75b4ff553ffe3b005ad72
lucasvinbr <lucasvinbr@gmail.com>
2015-04-27 12:59:16 -0300

    Script de mudar skin do player e prefab do player

---
+++ GameJam/Assets/Scripts/EscolheSkinDoPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// Escolhe skin do player. As skins sao animadores diferentes.
+/// </summary>
+public class EscolheSkinDoPlayer : MonoBehaviour {
+
+	public RuntimeAnimatorController[] skins;
+
+	// Use this for initialization
+	void Start () {
+	
+		GetComponent<Animator> ().runtimeAnimatorController = skins [Random.Range (0, skins.Length)];
+
+	}
+
+}
