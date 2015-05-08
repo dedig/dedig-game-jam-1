b1c5d3e843ae75994a64377fe55155b761c93995
Mauricio Cardozo <dedigmac1@Mauricios-Mac-mini.local>
2015-05-08 18:30:30 -0300

    modificação no menu principal e bugfixes

---
+++ GameJam/Assets/Scripts/HighScoreController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScoreController : MonoBehaviour {
+
+	int[] highScorePoints = new int[15];
+	string[] highScoreNames = new string[15];
+
+	public void Animar(){
+		Animator anim = gameObject.GetComponent<Animator>();
+		anim.Play("Move High Score");
+		anim.Play("HighScores");
+	}
+
+	void Start(){
+
+	}
+
+	void GetMySavedScores(){
+		for(int i = 0; i < 15; i++){
+			highScorePoints[i] = PlayerPrefs.GetInt(i.ToString());
+			highScoreNames[i] = PlayerPrefs.GetString (i.ToString());
+		}
+	}
+
+	void SortScores(){
+
+	}
+
+	public static void SetHighScore(string name, int points){
+
+	}
+}
--- GameJam/Assets/Scripts/UIMainMenu.cs
+++ GameJam/Assets/Scripts/UIMainMenu.cs
@@ -4,11 +4,12 @@ using UnityEngine.UI;
 
 public class UIMainMenu : MonoBehaviour {
 
-	public GameObject pauseMenu, scoreCanvas;
+	public GameObject pauseMenu, scoreCanvas, highScores, credits, mainMenu;
 	public GameObject somHabilitado, somDesabilitado;
+	public float tempoDeTrocaDeTela = 15;
 	static bool audioHabilitado = true;
-	float posicaoTexto = 1.0f;
-
+	int telaAtual = 0;
+	float deltaTime;
 
 	void Start(){
 		// Validação de menu de pausa
@@ -17,6 +18,11 @@ public class UIMainMenu : MonoBehaviour {
 			Debug.Break();
 		}
 		ControladorColunas.multiplicadorDaPontucao = 0;
+		if(PlayerPrefs.GetInt("som") == 0){
+			DesativarSom();
+		} else {
+			AtivarSom();
+		}
 	}
 
 	public void IniciaJogo(){
@@ -29,15 +35,57 @@ public class UIMainMenu : MonoBehaviour {
 
 	public void AlternarSom(){
 		if (audioHabilitado) {
-			AudioListener.volume = 0;
-			audioHabilitado = false;
-			somHabilitado.SetActive(false);
-			somDesabilitado.SetActive(true);
+			DesativarSom();
 		} else {
-			AudioListener.volume = 1;
-			audioHabilitado = true;
-			somHabilitado.SetActive(true);
-			somDesabilitado.SetActive(false);
+			AtivarSom();
+		}
+	}
+
+	void DesativarSom(){
+		AudioListener.volume = 0;
+		audioHabilitado = false;
+		somHabilitado.SetActive(false);
+		somDesabilitado.SetActive(true);
+		PlayerPrefs.SetInt("som", 0);
+	}
+
+	void AtivarSom(){
+		AudioListener.volume = 1;
+		audioHabilitado = true;
+		somHabilitado.SetActive(true);
+		somDesabilitado.SetActive(false);
+		PlayerPrefs.SetInt("som", 1);
+	}
+
+	void TrocarTela(){
+		switch(telaAtual){
+		case 0:
+			mainMenu.SetActive(false);
+			credits.SetActive(true);
+			break;
+		case 1:
+			credits.SetActive(false);
+			highScores.SetActive(true);
+			highScores.GetComponent<HighScoreController>().Animar();
+			break;
+		case 2:
+			highScores.SetActive(false);
+			mainMenu.SetActive(true);
+			telaAtual = -1;
+			break;
+		default:
+			telaAtual = 0;
+			break;
+		}
+		telaAtual++;
+	}
+
+	void Update(){
+		/* CHAMAR UMA COROUTINE PRA ISSO AQUI !!111 */
+		deltaTime += Time.deltaTime;
+		if(deltaTime > tempoDeTrocaDeTela){
+			deltaTime = 0;
+			TrocarTela();
 		}
 	}
 }
